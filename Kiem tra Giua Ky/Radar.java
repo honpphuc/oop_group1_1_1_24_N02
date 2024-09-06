f7d721b4a4b6f1698ee6af1f5ae9256eaa940989
honpphuc <23010746@st.phenikaa-uni.edu.vn>
2024-09-06 07:56:01 +0000

    Giua ky

--- Kiem tra Giua Ky/Radar.java
+++ Kiem tra Giua Ky/Radar.java
@@ -1,14 +1,15 @@
 public class Radar{
     public double X(int n){
-        if(n >= 0 && n <= 15)
-            return 1 - (n / 15);
+        if(n >= 0 && n <= 15){
+            return 1 - (double)n / 15;
+        }
         else
             return 0;
-        public static void main(String[] args){
-            Radar radar = new Radar();
-            int n = 4;
-            double result = Radar.X(n);
-            System.out.println("Result:" + result);
-        }
     }
+}
+public static void main(String[] args){
+    Radar radar  = new Radar();
+    int n = 4;
+    double result = radar.X(n);
+    System.out.println("X("+n+")="+result);
 }
