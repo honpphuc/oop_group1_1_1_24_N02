5048aec0bfc49e44684c2fe87513441ab485ebac
honpphuc <23010746@st.phenikaa-uni.edu.vn>
2024-09-06 08:16:51 +0000

    Giua Ky

--- Kiem tra Giua Ky/DiscreteSignal.java
+++ Kiem tra Giua Ky/DiscreteSignal.java
@@ -1,3 +1,12 @@
-public DiscreteSignal{
-    
+public class Discrete Signal{
+    private double[] samples;
+    public DiscreteSignal(double[] samples){
+        this.samples = samples;
+    }
+    public double getvalue(int n){
+        if(n < 0 || n >samples.length)
+            return samples[n];
+        else
+            return 0;
+    }
 }
